0f8cc97642f0cc659e7cac0218c491bf10e8f4c2
Rohit Mukkala <mukkalarohit@gmail.com>
2024-09-14 00:37:09 +0530

    Completed till mic setup

--- www/main.js
+++ www/main.js
@@ -9,4 +9,36 @@ $(document).ready(function () {
       effect: "bounceOut",
     },
   });
+  // Siri configuration
+  var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 800,
+    height: 200,
+    style: "ios9",
+    amplitude: "1",
+    speed: 0.3,
+    autostart: true,
+  });
+
+  // Siri Message Animation
+
+  $(".siri-message").textillate({
+    loop: true,
+    sync: true,
+    in: {
+      effect: "fadeInUp",
+      sync: true,
+    },
+    out: {
+      effect: "fadeOutUp",
+      sync: true,
+    },
+  });
+
+  // Mic Button click Event
+  $("#MicBtn").click(function () {
+    eel.playAssistantSound();
+    $("#oval").attr("hidden", true);
+    $("#SiriWave").attr("hidden", false);
+  });
 });
