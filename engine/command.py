594f0f0142786bd5030c95436137085eb70ec39f
Rohit Mukkala <mukkalarohit@gmail.com>
2024-11-11 23:26:22 +0530

    Corrected sound

--- engine/command.py
+++ engine/command.py
@@ -11,7 +11,7 @@ def speak(text):
     engine = pyttsx3.init("sapi5")
     voices = engine.getProperty("voices")
     engine.setProperty("voice", voices[0].id)
-    engine.setProperty('rate', 174)
+    engine.setProperty('rate', 150)
     eel.DisplayMessage(text)
     engine.say(text)
     eel.receiverText(text)
